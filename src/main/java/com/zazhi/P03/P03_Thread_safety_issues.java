3006f900bcb578b535a59356418c6778d5a25d87
zazhi <2065707071@qq.com>
2025-04-17 21:57:48 +0800

    feat: 线程安全问题

---
+++ src/main/java/com/zazhi/P03/P03_Thread_safety_issues.java
@@ -0,0 +1,75 @@
+package com.zazhi.P03;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
+/**
+ * @author zazhi
+ * @date 2025/4/17
+ * @description: 线程安全问题 -- 卖票问题
+ */
+@Slf4j
+public class P03_Thread_safety_issues {
+
+    public static void main(String[] args) {
+        TicketWindow ticketWindow = new TicketWindow(2000);
+        List<Thread> list = new ArrayList<>();
+        // 用来存储买出去多少张票
+        List<Integer> sellCount = new Vector<>();
+        for (int i = 0; i < 8000; i++) {
+            Thread t = new Thread(() -> {
+                // 分析这里的竞态条件
+                int count = ticketWindow.sell(randomAmount());
+                sellCount.add(count);
+            });
+            list.add(t);
+            t.start();
+        }
+        list.forEach((t) -> {
+            try {
+                t.join();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        log.debug("卖票数量: {}",sellCount.stream().mapToInt(c -> c).sum());
+        log.debug("剩余票数: {}", ticketWindow.getCount());
+    }
+
+    static Random random = new Random(); // Random 为线程安全
+    public static int randomAmount() {
+        return random.nextInt(5) + 1;
+    }
+}
+
+class TicketWindow {
+    private int count;
+    public TicketWindow(int count) {
+        this.count = count;
+    }
+    public int getCount() {
+        return count;
+    }
+    public synchronized int sell(int amount) { // 加锁
+//    public int sell(int amount) {
+        if (this.count >= amount) {
+
+            try {
+                Thread.sleep(1); // 模拟卖票的耗时, 模拟高并发情况下指令交错
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            this.count -= amount;
+            return amount;
+        }
+        else {
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/com/zazhi/P03/P03_Thread_safety_issues_02.java
@@ -0,0 +1,66 @@
+package com.zazhi.P03;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+
+/**
+ * @author zazhi
+ * @date 2025/4/17
+ * @description: 线程安全问题 -- 转账问题
+ */
+@Slf4j
+public class P03_Thread_safety_issues_02 {
+    public static void main(String[] args) throws InterruptedException {
+        Account a = new Account(1000);
+        Account b = new Account(1000);
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                a.transfer(b, randomAmount());
+            }
+        },"t1");
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                b.transfer(a, randomAmount());
+            }
+        },"t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        // 查看转账2000次后的总金额
+        log.debug("账户 A: {}", a.getMoney());
+        log.debug("账户 B: {}", b.getMoney());
+    }
+
+    static Random random = new Random();
+
+    public static int randomAmount() {
+        return random.nextInt(100) + 1;
+    }
+}
+
+class Account {
+    private int money;
+
+    public Account(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void transfer(Account target, int amount) {
+        synchronized (Account.class) { // 锁定类对象
+            if (this.money > amount) {
+                this.setMoney(this.getMoney() - amount);
+                target.setMoney(target.getMoney() + amount);
+            }
+        }
+    }
+}
